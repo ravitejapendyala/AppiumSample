2961fc9a992bdfa00ada313273eca4fcbd74b3b2
ravitejapendyala <VenkataRaviTeja.Pendyala@blueyonder.com>
2023-08-31 18:57:38 +0530

    Creating driver session

--- src/test/java/StartDriverSession.java
+++ src/test/java/StartDriverSession.java
@@ -1,11 +1,31 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class StartDriverSession {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
 
         DesiredCapabilities caps = new DesiredCapabilities();
+
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME,"pixel_7_pro");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        caps.setCapability(MobileCapabilityType.UDID,"emulator-5554");
+        String appUrl = System.getProperty("user.dir")+ File.separator+"src"+ File.separator+"main"+ File.separator+"resources"+ File.separator+"ApiDemos-debug.apk";
+        //caps.setCapability(MobileCapabilityType.APP,appUrl);
+        caps.setCapability("appPackage","io.appium.android.apis");
+        caps.setCapability("appActivity","io.appium.android.apis.accessibility.CustomViewAccessibilityActivity");
+
+        URL url = new URL("http://0.0.0.0:4723");
+
+        AppiumDriver driver = new AndroidDriver(url,caps);
 
     }
 
--- src/test/java/StartDriverSessionUsingOptions.java
+++ src/test/java/StartDriverSessionUsingOptions.java
@@ -1,5 +1,6 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -7,23 +8,20 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class StartDriverSession {
+public class StartDriverSessionUsingOptions {
 
 
     public static void main(String[] args) throws MalformedURLException {
 
-        DesiredCapabilities caps = new DesiredCapabilities();
-
-        caps.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
-        caps.setCapability(MobileCapabilityType.DEVICE_NAME,"pixel_7_pro");
-        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        caps.setCapability(MobileCapabilityType.UDID,"emulator-5554");
         String appUrl = System.getProperty("user.dir")+ File.separator+"src"+ File.separator+"main"+ File.separator+"resources"+ File.separator+"ApiDemos-debug.apk";
-        caps.setCapability(MobileCapabilityType.APP,appUrl);
+        UiAutomator2Options opt = new UiAutomator2Options().
+                setDeviceName("pixel_7_pro").
+                setAutomationName("UiAutomator2").
+                setApp(appUrl);
 
         URL url = new URL("http://0.0.0.0:4723");
 
-        AppiumDriver driver = new AndroidDriver(url,caps);
+        AppiumDriver driver = new AndroidDriver(url,opt);
 
     }
 
