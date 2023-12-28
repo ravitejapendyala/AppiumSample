f5dc92bd019d42e97a7e08afe60eb00e1f9bda76
ravitejapendyala <VenkataRaviTeja.Pendyala@blueyonder.com>
2023-12-28 13:31:52 +0530

    Getting attribute values  like checked,selected,displayed

---
+++ src/test/java/FetchElementAttributes.java
@@ -0,0 +1,34 @@
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+public class FetchElementAttributes {
+    public static void main(String[] args) throws Exception {
+
+        AppiumDriver driver = CreateDriverSession.initializeDriver("Android");
+
+        By Preference = AppiumBy.accessibilityId("Preference");
+        By switch_control = AppiumBy.accessibilityId("9. Switch");
+        By editText = AppiumBy.id("io.appium.android.apis:id/edit");
+        driver.findElement(Preference).click();
+        driver.findElement(switch_control).click();
+        By switchPref = AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.Switch\")");
+        By switchPref_text = AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.TextView\")");
+
+
+        System.out.println("text by text: "+driver.findElement(switchPref_text).getText());
+        System.out.println("text by attribute: "+driver.findElement(switchPref_text).getAttribute("text"));
+        System.out.println("checked status of pref1: "+driver.findElements(switchPref).get(0).getAttribute("checked"));
+        System.out.println("checked status of pref1: "+driver.findElements(switchPref).get(1).getAttribute("checked"));
+        /*
+        System.out.println("enabled status: "+driver.findElement(Preference).getAttribute("enabled"));
+        System.out.println("selected status: "+driver.findElement(Preference).getAttribute("selected"));
+        System.out.println("displayed status: "+driver.findElement(Preference).getAttribute("displayed"));
+        System.out.println("isEnabled status: "+driver.findElement(Preference).isEnabled());
+*/
+
+    }
+}
