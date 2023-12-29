6295af66758147aee85affacf4aa011dd00d549e
ravitejapendyala <VenkataRaviTeja.Pendyala@blueyonder.com>
2023-12-29 16:53:41 +0530

    Adding ScrollToBottom

--- src/test/java/Gestures_Android_Automation/ScrollGesture.java
+++ src/test/java/Gestures_Android_Automation/ScrollGesture.java
@@ -32,7 +32,9 @@ public class ScrollGesture {
         Thread.sleep(3000);
 //        SwipeByElement(driver);
         //ScrollByCoordinates(driver);
-        SwipeByElement(driver);
+//        ScrollByElement(driver);
+        //ScrollOnElement(driver);
+        ScrollToBottom(driver);
 //        ScrollToEndHorizontally(driver,"right");
 //        Thread.sleep(3000);
 //        ScrollToEndHorizontally(driver,"left");
@@ -48,7 +50,7 @@ public class ScrollGesture {
                 "percent",1.0
         ));
     }
-    public  static void SwipeByElement(AppiumDriver driver)
+    public  static void ScrollByElement(AppiumDriver driver)
     {
         WebElement element = driver.findElement(AppiumBy.id("android:id/list"));
         boolean canScroll  = (Boolean)((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
@@ -57,6 +59,52 @@ public class ScrollGesture {
                 "direction","down",
                 "percent",1.0
         ));
+
+//        By photos = AppiumBy.accessibilityId("1. Photos");
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(photos)).click();
+//        WebElement element = driver.findElement(AppiumBy.xpath("//*[@resource-id=\"io.appium.android.apis:id/gallery\"]/android.widget.ImageView[1]"));
+//        ((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+//                //"left",701, "top",1600, "width",700, "height",1100,
+//                "elementId",((RemoteWebElement)element).getId(),
+//                "direction","left",
+//                "percent",0.75
+//        ));
+    }
+    public  static void ScrollOnElement(AppiumDriver driver)
+    {
+        WebElement element = driver.findElement(AppiumBy.accessibilityId("Animation"));
+        boolean canScroll  = (Boolean)((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                "elementId",((RemoteWebElement)element).getId(),
+                //"left",100, "top",100, "width",600, "height",600,
+                "direction","down",
+                "percent",1.0
+        ));
+
+//        By photos = AppiumBy.accessibilityId("1. Photos");
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(photos)).click();
+//        WebElement element = driver.findElement(AppiumBy.xpath("//*[@resource-id=\"io.appium.android.apis:id/gallery\"]/android.widget.ImageView[1]"));
+//        ((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+//                //"left",701, "top",1600, "width",700, "height",1100,
+//                "elementId",((RemoteWebElement)element).getId(),
+//                "direction","left",
+//                "percent",0.75
+//        ));
+    }
+    public  static void ScrollToBottom(AppiumDriver driver)
+    {
+        WebElement element = driver.findElement(AppiumBy.accessibilityId("Animation"));
+        boolean canScrollMore = true;
+        while(canScrollMore){
+            canScrollMore  = (Boolean)((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                    //"elementId",((RemoteWebElement)element).getId(),
+                    "left",100, "top",100, "width",600, "height",600,
+                    "direction","down",
+                    "percent",1.0
+            ));
+            System.out.println("Can scroll: "+canScrollMore);
+
+        }
+
 
 //        By photos = AppiumBy.accessibilityId("1. Photos");
 //        wait.until(ExpectedConditions.visibilityOfElementLocated(photos)).click();
